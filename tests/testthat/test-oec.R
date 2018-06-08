12c61178889219ba6f821c2dfa75991c56eed394
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-06-08 16:50:02 -0400

    maelle feedback

--- tests/testthat/test-oec.R
+++ tests/testthat/test-oec.R
@@ -14,33 +14,7 @@ test_that("getdata connects to the API and returns a valid tibble after valid in
 
 test_that("getdata connects to the API and returns an error after invalid input", {
   if (curl::has_internet()) {
-    expect_error(getdata("chl", "arg", 1980, "hs92"), "year >= 1992 is not TRUE")
-    expect_error(getdata("chl", "arg", 1980, "hs96"), "year >= 1996 is not TRUE")
-    expect_error(getdata("chl", "arg", 1980, "hs02"), "year >= 2002 is not TRUE")
-    expect_error(getdata("chl", "arg", 1980, "hs07"), "year >= 2007 is not TRUE")
-  } else {
-    skip("No internet connection")
-  }
-})
-
-test_that("getdata_batch connects to the API and returns a valid tibble after valid input", {
-  if (curl::has_internet()) {
-    # Bilateral trade Chile-Argentina (SITC, 1980-1981)
-    test_data_2 <- getdata_batch("chl", "arg", 1980, 1981)
-    expect_is(test_data_2, "tbl")
-    expect_is(test_data_2, "data.frame")
-    expect_output(str(test_data_2), "30 variables")
-  } else {
-    skip("No internet connection")
-  }
-})
-
-test_that("getdata_batch connects to the API and returns an error after invalid input", {
-  if (curl::has_internet()) {
-    expect_error(getdata_batch("chl", "arg", 1980, 1981, "hs92"), "year >= 1992 is not TRUE")
-    expect_error(getdata_batch("chl", "arg", 1980, 1981, "hs96"), "year >= 1996 is not TRUE")
-    expect_error(getdata_batch("chl", "arg", 1980, 1981, "hs02"), "year >= 2002 is not TRUE")
-    expect_error(getdata_batch("chl", "arg", 1980, 1981, "hs07"), "year >= 2007 is not TRUE")
+    expect_error(getdata("chl", "arg", 1980, "hs92"), "all\\(years >= 1992\\) is not TRUE")
   } else {
     skip("No internet connection")
   }
--- vignettes/oec.R
+++ vignettes/oec.R
@@ -4,12 +4,12 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----countries-----------------------------------------------------------
+## ----countries, eval = TRUE----------------------------------------------
 library(oec)
 
 country_codes
 
-## ----products------------------------------------------------------------
+## ----products, eval = TRUE-----------------------------------------------
 hs92
 
 hs96
@@ -20,83 +20,33 @@ hs07
 
 sitc
 
-## ----getdata-------------------------------------------------------------
+## ----getdata, eval = TRUE------------------------------------------------
 # What does Chile exchange with China?  
 # year 2015 - SITC (4 characters)
 getdata("chl", "chn", 2015)
 
-## ----getdata2, eval = F--------------------------------------------------
-#  # What does Chile exchange with China?
-#  # year 2015 - HS07 (4 and 6 characters)
-#  getdata("chl", "chn", 2015, "hs92")
-#  
-#  # What does Chile exchange with China?
-#  # year 2015 - HS96 (4 and 6 characters)
-#  getdata("chl", "chn", 2015, "hs96")
-#  
-#  # What does Chile exchange with China?
-#  # year 2015 - HS02 (4 and 6 characters)
-#  getdata("chl", "chn", 2015, "hs02")
-#  
-#  # What does Chile exchange with China?
-#  # year 2015 - HS07 (4 and 6 characters)
-#  getdata("chl", "chn", 2015, "hs07")
+## ----getdata2, eval = TRUE-----------------------------------------------
+# What does Chile exchange with China?  
+# year 2015 - HS07 (4 and 6 characters)
+getdata("chl", "chn", 2015, "hs07")
 
-## ----getdata3------------------------------------------------------------
-chl_chn_2015_sitc
+## ----getdata2_2, eval = TRUE---------------------------------------------
+# What does Chile exchange with China?  
+# years 2010 and 2015 - HS07 (4 and 6 characters)
+getdata("chl", "chn", c(2010,2015), "hs07")
 
-## ----getdata4, eval = F--------------------------------------------------
-#  # What does Chile exchange with China?
-#  # year 2000 - HS07 (4 and 6 characters)
-#  getdata("chl", "chn", 2000, "hs02")
+## ----getdata3, eval = TRUE, error=TRUE-----------------------------------
+# What does Chile exchange with China?  
+# year 2000 - HS02 (4 and 6 characters)
+getdata("chl", "chn", 2000, "hs02")
 
-## ----getdata5, eval = F--------------------------------------------------
-#  # What does Andorra exchange with France?
-#  # year 2015 - SITC (4 characters)
-#  getdata("and", "fra", 2015)
+## ----getdata4, eval = TRUE, error = TRUE---------------------------------
+# What does Andorra exchange with France?  
+# year 2015 - SITC (4 characters)
+getdata("and", "fra", 2015)
 
-## ----getdata6, eval = F--------------------------------------------------
-#  # What does Andorra exchange with France?
-#  # year 2015 - HS07 (4 and 6 characters)
-#  getdata("and", "fra", 2015, "hs07")
-
-## ----getdata_batch, eval = F---------------------------------------------
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - SITC (4 characters)
-#  getdata_batch("chl", "chn", 2010, 2015)
-
-## ----getdata_batch2, eval = F--------------------------------------------
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - SITC (4 characters)
-#  getdata("chl", "chn", 2010)
-#  getdata("chl", "chn", 2011)
-#  getdata("chl", "chn", 2012)
-#  getdata("chl", "chn", 2013)
-#  getdata("chl", "chn", 2014)
-#  getdata("chl", "chn", 2015)
-#  
-#  data <- bind_rows(chl_chn_2010_sitc, chl_chn_2011_sitc, chl_chn_2012_sitc,
-#                    chl_chn_2013_sitc, chl_chn_2014_sitc, chl_chn_2015_sitc)
-
-## ----getdata_batch3, eval = F--------------------------------------------
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - HS92 (4 and 6 characters)
-#  getdata_batch("chl", "chn", 2010, 2015, "hs92")
-#  
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - HS96 (4 and 6 characters)
-#  getdata_batch("chl", "chn", 2010, 2015, "hs96")
-#  
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - HS02 (4 and 6 characters)
-#  getdata_batch("chl", "chn", 2010, 2015, "hs02")
-#  
-#  # What does Chile exchange with China?
-#  # years 2010 to 2015 - HS07 (4 and 6 characters)
-#  getdata_batch("chl", "chn", 2010, 2015, "hs07")
-
-## ----getdata_batch4, eval = F--------------------------------------------
-#  # What does Chile exchange with China?
-#  # years 2010, 2012 and 2014 - HS07 (4 and 6 characters)
-#  getdata_batch("chl", "chn", 2010, 2014, "hs07", 2)
+## ----getdata5, eval = TRUE-----------------------------------------------
+# What does Andorra exchange with France?  
+# year 2015 - HS07 (4 and 6 characters)
+getdata("and", "fra", 2015, "hs07")
 
